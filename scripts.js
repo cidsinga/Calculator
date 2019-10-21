7b13e2e302b0705b3dd77fba0fea3424f321b5c8
Casey Idsinga and Marcus Jensen <casey@>
2019-10-21 11:02:28 -0700

    build JS file per project requirements

--- scripts.js
+++ scripts.js
@@ -1,10 +1,38 @@
-var add = function(number1, number2) {
-  return number1 + number2;
-};
+// var add = function(number1, number2) {
+//   return number1 + number2;
+// };
+//
+var number1 = parseInt(prompt("Enter tempurature in celcius"));
+//var number2 = parseInt(prompt("Enter your height (m)"));
+//
+// // alert(add(number1, number2));
+//
+// var subtract = function(number1, number2) {
+//   return number1 - number2;
+// };
+//
+// var multiply = function(number1, number2) {
+//   return number1 * number2;
+// };
+//
+// var divide = function(number1, number2) {
+//   return number1 / number2;
+// };
+//
+// alert(multiply(number1, number2));
 
-var number1 = parseInt(prompt("Enter a number:"));
-var number2 = parseInt(prompt("Enter another number:"));
+// var bmi = function(){
+//
+//   var squareHeight = number2 * number2;
+//   var result = number1 / squareHeight;
+//   alert(result);
+// };
+//
+// bmi();
 
-alert(add(number1, number2));
+var tempurature = function(){
+  var fahrenheit = number1 * 9/5 + 32;
+  alert(fahrenheit);
 
-console.log('hey');
+}
+tempurature()
